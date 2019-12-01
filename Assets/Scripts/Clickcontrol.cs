cd3f0b27e711d65fb5134d3429b2f8b325ee3166
friskameilani <friska.meilani19@gmail.com>
2019-12-01 22:40:58 +0700

    test

---
+++ Assets/Scripts/Clickcontrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clickcontrol : MonoBehaviour
+{
+	public static string nameofobj;
+	public GameObject objnametext;
+	public GameObject scoreText;
+	public static int theScore;
+	
+	void Start(){
+        
+	}
+
+	void Update(){
+        
+	}
+
+	void OnMouseDown(){
+        nameofobj = gameObject.name;
+        Debug.Log(nameofobj);
+        Destroy(gameObject);
+        Destroy(objnametext);
+	ScoringSystem.theScore += 50;
+	}
+}
